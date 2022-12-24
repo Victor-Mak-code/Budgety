ee18953489e8ac78c38d9a41f332d0049fa3f06e
victor mak <thegreentechoffice@gmail.com>
2022-12-25 00:04:29 +0100

    added and styled the expenseUI

---
+++ src/Components/AddExpenseUI/AddExpenseUI.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./AddExpenseUI.css";
+
+const AddExpenseUI = () => {
+  return (
+    <div className="add__expense-box">
+      <h2>Create Expenses</h2>
+      <p>Add your day to day spendings to get started</p>
+    </div>
+  );
+};
+
+export default AddExpenseUI;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import Hero from "../../Components/Hero/Hero";
+import AddExpenseUI from "../../Components/AddExpenseUI/AddExpenseUI";
 
 const Home = () => {
   return (
     <div className="home">
       <Hero />
+      <AddExpenseUI />
     </div>
   );
 };
