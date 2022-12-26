42f4f4ab462f8ad242e391a357fbbf5dfa89ea3b
victor mak <thegreentechoffice@gmail.com>
2022-12-26 12:45:38 +0100

    styled the signup component

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,8 +1,34 @@
 import React from "react";
-import "./SignUp.css";
+import FormInput from "../../Components/FormInput/FormInput";
+import Button from "../../Components/Button/Button";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="form__auth">
+      <div className="form_auth__header">
+        <div className="logo">
+          <span>Budgety.</span>
+        </div>
+      </div>
+
+      <div className="form__auth__body">
+        <h2 className="heading__2">Sign Up</h2>
+        <p>Aleady have an account, Sign In </p>
+        <form>
+          <FormInput
+            labelValue="Budget Name"
+            placeholder="eg. Monthly Budget"
+          />
+          <FormInput
+            labelValue="Email Address"
+            placeholder="eg. johnsmith@gmail.com"
+          />
+          <FormInput labelValue="Password" placeholder="**********" />
+          <Button btnValue="Sign Up" btnClass="btn btn-primary" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
