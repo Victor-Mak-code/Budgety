9587f773d828634680fd60bcba02102486ff111e
victor mak <thegreentechoffice@gmail.com>
2023-01-06 23:53:05 +0100

    added validateFormData folder

--- src/ActionHandlers/SignupHandler/SignupHandler.js
+++ src/ActionHandlers/SignupHandler/SignupHandler.js
@@ -1,20 +1,23 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
+import validateFormData from "../ValidateFormData/ValidateFormData";
 
 const signUpHandler = async (email, password) => {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      console.log(user);
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorMessage);
-      // ..
-    });
+  if (validateFormData(email, password)) {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        // ..
+      });
+  }
 };
 
 export { signUpHandler };
